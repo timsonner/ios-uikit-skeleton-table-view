eeaf6bd66925bf73ed7fb8f3e061da632d9bc8e5
timsonner <66705347+timsonner@users.noreply.github.com>
2022-10-24 19:12:42 -0600

    add finished project

--- Skeleton Table View/ViewController.swift
+++ Skeleton Table View/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Connect table view to view controller.
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Declare view controller as the delegate
+        tableView.delegate = self
+        // Declare view controller as the data source
+        tableView.dataSource = self
     }
-
+    
+    // Provide conformance to ui table view delegate and ui table view data source.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    // Provide conformance to ui table view delegate and ui table view data source.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create instance of ui table view cell.
+        let cell = UITableViewCell()
+        // Create content object with a default configuration.
+        var content = cell.defaultContentConfiguration()
+        // Set text property of the content object.
+        content.text = "text"
+        content.secondaryText = "secondaryText"
+        // Asign content configuration to the instance of ui table view cell.
+        cell.contentConfiguration = content
+        // Return the ui table view cell instance
+        return cell
+    }
 
 }
 
